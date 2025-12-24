5a5fb871c5709680002aedc6522e5da32f13106b
rafipsv <rafidiya143@gmail.com>
2025-12-24 12:25:19 +0600

    Overflow Issue Solved

---
+++ lib/core/data/local/storage_service.dart
@@ -0,0 +1,26 @@
+import 'package:get_storage/get_storage.dart';
+
+class StorageService {
+  final _box = GetStorage();
+  final String tokenKey = "token";
+
+  // Read
+  T? read<T>({required String key}) {
+    return _box.read(key);
+  }
+
+  // Write
+  Future<void> write({required String key, required dynamic value}) async {
+    await _box.write(key, value);
+  }
+
+  // Remove
+  Future<void> remove({required String key}) async {
+    await _box.remove(key);
+  }
+
+  // Clear all
+  Future<void> clear() async {
+    await _box.erase();
+  }
+}
