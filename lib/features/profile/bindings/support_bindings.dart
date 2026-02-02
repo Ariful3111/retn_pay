0ae85cad9cab4254c3b33aa0e5dd875c6b40d57f
rafipsv <rafidiya143@gmail.com>
2026-02-02 13:38:42 +0600

    Modified

--- lib/features/profile/bindings/support_bindings.dart
+++ lib/features/profile/bindings/support_bindings.dart
@@ -1,8 +1,13 @@
 import 'package:get/get.dart';
 import 'package:renter_pay/features/profile/controllers/support_controller.dart';
+import 'package:renter_pay/features/rent/controllers/faq_controller.dart';
+import 'package:renter_pay/features/rent/repositories/faq_repo.dart';
 
-class SupportBindings implements Bindings{
+class SupportBindings implements Bindings {
   @override
   void dependencies() {
-Get.lazyPut(()=>SupportController());  }
+    Get.lazyPut(() => FAQRepository(getNetwork: Get.find()));
+    Get.lazyPut(() => SupportController());
+    Get.lazyPut(() => FAQController(faqRepository: Get.find()));
+  }
 }
