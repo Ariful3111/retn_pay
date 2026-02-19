ed33ac1b6d3bc6e82b75d1a686228a8c49f17791
rafipsv <rafidiya143@gmail.com>
2026-02-19 13:07:41 +0600

    Modified

--- lib/features/dashboard/bindings/services_bindings.dart
+++ lib/features/dashboard/bindings/services_bindings.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
 import 'package:renter_pay/features/dashboard/controllers/tenant_controller/services_controller.dart';
+import 'package:renter_pay/features/dashboard/controllers/tenant_controller/submit_property_review_controller.dart';
+import 'package:renter_pay/features/dashboard/repositories/landlord_repositories/submit_property_review_repo.dart';
 
 class ServicesBindings implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => ServicesController());
+    Get.lazyPut(() => SubmitPropertyReviewRepository(postNetwork: Get.find()));
+    Get.lazyPut(
+      () => SubmitPropertyReviewController(
+        submitPropertyReviewRepository: Get.find(),
+      ),
+    );
   }
 }
