5be27c1dbac9a8c9b7d7192428d0b24a4f8af27e
rafipsv <rafidiya143@gmail.com>
2026-02-25 10:00:54 +0600

    Modified

--- lib/features/dashboard/views/landlord_views/property_management.dart
+++ lib/features/dashboard/views/landlord_views/property_management.dart
@@ -71,13 +71,14 @@ class PropertyManagement extends StatelessWidget {
             } else {
               child = Column(
                 children: [
-                if(!propertyManagementController.isViewProperty.value)  Column(
-                    children: [
-                      SizedBox(height: 16.h),
-                      PropertyManagementRow(),
-                      SizedBox(height: 20.h),
-                    ],
-                  ),
+                  if (!propertyManagementController.isViewProperty.value)
+                    Column(
+                      children: [
+                        SizedBox(height: 16.h),
+                        PropertyManagementRow(),
+                        SizedBox(height: 20.h),
+                      ],
+                    ),
                   propertyManagementController.isViewProperty.value
                       ? PropertyManagementDetails()
                       : PropertyManagementTable(),
