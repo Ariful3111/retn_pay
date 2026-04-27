22eb67a1bc35d0786839a1caba48d2f1bc5681c9
Ariful3111 <arifulislamdurjoy3111@gmail.com>
2026-04-27 17:47:09 +0600

    checkout pages done

--- lib/features/checkout/controller/withdraw_controller.dart
+++ lib/features/checkout/controller/withdraw_controller.dart
@@ -1,19 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:renter_pay/features/profile/controllers/balance_controller.dart';
-import 'package:renter_pay/features/profile/repositories/withdraw_repo.dart';
+import 'package:renter_pay/features/checkout/controller/balance_controller.dart';
+import 'package:renter_pay/features/checkout/repositories/withdraw_repo.dart';
 import 'package:renter_pay/shared/widgets/snackbars/error_snackbar.dart';
 import 'package:renter_pay/shared/widgets/snackbars/success_snackbar.dart';
 
 class WithdrawController extends GetxController {
   final WithdrawRepository withdrawRepository;
   WithdrawController({required this.withdrawRepository});
-
+  TextEditingController withdrawController = TextEditingController();
   RxBool isLoading = false.obs;
 
   Future<void> withdraw() async {
     isLoading.value = true;
+    double withdrawAmount = double.tryParse(withdrawController.text) ?? 0.0;
     final response = await withdrawRepository.execute(
-      amount: 50,
+      amount: withdrawAmount,
       currency: 'AUD',
       paymentMethod: 1,
     );
--- lib/features/profile/bindings/profile_bindings.dart
+++ lib/features/profile/bindings/profile_bindings.dart
@@ -1,15 +1,11 @@
 import 'package:get/get.dart';
 import 'package:renter_pay/features/auth/controllers/logout_controller.dart';
 import 'package:renter_pay/features/auth/repositories/logout_repo.dart';
-import 'package:renter_pay/features/profile/controllers/balance_controller.dart';
+import 'package:renter_pay/features/checkout/controller/balance_controller.dart';
 import 'package:renter_pay/features/profile/controllers/privacy_controller.dart';
 import 'package:renter_pay/features/profile/controllers/profile_controller.dart';
-import 'package:renter_pay/features/profile/controllers/start_deposit_controller.dart';
-import 'package:renter_pay/features/profile/controllers/withdraw_controller.dart';
 import 'package:renter_pay/features/profile/repositories/get_balance_repo.dart';
 import 'package:renter_pay/features/profile/repositories/get_profile_repo.dart';
-import 'package:renter_pay/features/profile/repositories/start_deposit_repo.dart';
-import 'package:renter_pay/features/profile/repositories/withdraw_repo.dart';
 
 class ProfileBindings implements Bindings {
   @override
@@ -17,18 +13,12 @@ class ProfileBindings implements Bindings {
     // Repositories
     Get.lazyPut(() => GetProfileRepository(getNetwork: Get.find()));
     Get.lazyPut(() => LogoutRepository(postWithoutResponse: Get.find()));
-    Get.lazyPut(() => StartDepositRepository(postWithoutResponse: Get.find()));
     Get.lazyPut(() => GetBalanceRepository(getNetwork: Get.find()));
-    Get.lazyPut(() => WithdrawRepository(postWithoutResponse: Get.find()));
 
     // Controllers
     Get.lazyPut(() => ProfileController(getProfileRepository: Get.find()));
     Get.lazyPut(() => LogoutController(logoutRepository: Get.find()));
     Get.lazyPut(() => PrivacyController());
-    Get.lazyPut(
-      () => StartDepositController(startDepositRepository: Get.find()),
-    );
     Get.lazyPut(() => BalanceController(getBalanceRepository: Get.find()));
-    Get.lazyPut(() => WithdrawController(withdrawRepository: Get.find()));
   }
 }
